c863a0e742e1f9d1763730be0d5c328866775bc9
224247 Andryskowski Stanisław <224247@edu.p.lodz.pl>
2020-08-27 14:35:53 +0200

    navbar added

--- script.js
+++ script.js
@@ -1,38 +1,27 @@
 const BURGER = document.querySelector(".fa-bars");
-const MENU = document.querySelector(".menu");
+const NAVBAR = document.querySelector(".navbar");
 const PRODUCTS = document.querySelector(".products");
-
-// if(menu.style.display=="none"){
-// burger.classList.toggle("mystyle");
-// }
-// else
-// {
-//     burger.classList.toggle("mystyle");
-// }
 
 BURGER.onclick = showOrNotShowMenu;
 PRODUCTS.onclick = showOrNotShowMenuForProducts;
 
-
 function showOrNotShowMenu(){
-    if(MENU.style.display=="block"){
-        MENU.style.display="none";
-        BURGER.style.display="block";
-
-    // PRODUCTS.style.marginTop="0";
+    if(NAVBAR.style.display=="block"){
+        NAVBAR.style.display="none";
+       
 }
 else
 {
-    MENU.style.display="block";
-    MENU.style.width="100vw";
-    BURGER.style.display="none";
-    // PRODUCTS.style.marginTop="250px";
+    NAVBAR.style.display="block";
+   
 }
 }
 
 function showOrNotShowMenuForProducts(){
-    if(MENU.style.display=="block"){
-        MENU.style.display="none";
-        BURGER.style.display="block";
+    if(NAVBAR.style.display=="block"){
+        NAVBAR.style.display="none";
+        
+        
     }
 }
+
